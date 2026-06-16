607b222ff5b40e0b322d75481e915312fdd731f2
pixverseofficial <pixverse.official00@gmail.com>
2026-06-16 18:29:40 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("Faith Is Fuel Loaded");
+window.addEventListener("load",()=>{
+
+    document.querySelector(".hero-content").animate(
+    [
+        {
+            opacity:0,
+            transform:"translateY(60px)"
+        },
+        {
+            opacity:1,
+            transform:"translateY(0)"
+        }
+    ],
+    {
+        duration:1500,
+        easing:"ease-out",
+        fill:"forwards"
+    });
+
+});
