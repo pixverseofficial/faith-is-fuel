4ba265751056c8bc86e2cae9e9687113aac42b0b
vex.designs <pixverse.official00@gmail.com>
2026-06-18 16:37:37 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getFirestore,
+collection,
+addDoc,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjPJbcZ1XrZvd0WB7XPDnFEGu9cIfN_1k",
+  authDomain: "faith-is-fuel.firebaseapp.com",
+  projectId: "faith-is-fuel",
+  storageBucket: "faith-is-fuel.firebasestorage.app",
+  messagingSenderId: "710775780373",
+  appId: "1:710775780373:web:4443f05813f6dd6caf0a1c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs };
