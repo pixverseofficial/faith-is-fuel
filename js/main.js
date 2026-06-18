70748f1e228fafd265259c6e930fb4da8fa06f85
pixverseofficial <pixverse.official00@gmail.com>
2026-06-18 06:24:22 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,28 +18,3 @@ window.addEventListener("load",()=>{
     });
 
 });
-document.querySelectorAll("a").forEach(link => {
-
-    link.addEventListener("click", function(e){
-
-        const href = this.getAttribute("href");
-
-        if(
-            href &&
-            !href.startsWith("#") &&
-            !href.startsWith("http")
-        ){
-
-            e.preventDefault();
-
-            document.querySelector(".page-loader")
-            .style.display = "flex";
-
-            setTimeout(() => {
-                window.location.href = href;
-            }, 500);
-        }
-
-    });
-
-});
