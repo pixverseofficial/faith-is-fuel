bc88861d08b22d063e7012fdfbef69d45bd84a60
vex.designs <pixverse.official00@gmail.com>
2026-06-18 17:15:23 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,16 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
 getFirestore,
 collection,
-addDoc
+addDoc,
+getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjPJbcZ1XrZvd0WB7XPDnFEGu9cIfN_1k",
-  authDomain: "faith-is-fuel.firebaseapp.com",
-  projectId: "faith-is-fuel",
-  storageBucket: "faith-is-fuel.firebasestorage.app",
-  messagingSenderId: "710775780373",
-  appId: "1:710775780373:web:4443f05813f6dd6caf0a1c"
+apiKey: "AIzaSyDjPJbcZ1XrZvd0WB7XPDnFEGu9cIfN_1k",
+authDomain: "faith-is-fuel.firebaseapp.com",
+projectId: "faith-is-fuel",
+storageBucket: "faith-is-fuel.firebasestorage.app",
+messagingSenderId: "710775780373",
+appId: "1:710775780373:web:4443f05813f6dd6caf0a1c"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,5 +23,6 @@ const db = getFirestore(app);
 export {
 db,
 collection,
-addDoc
+addDoc,
+getDocs
 };
