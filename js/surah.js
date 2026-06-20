8e0e38c4f350b1105dbf2a1d421331e54575d793
vex.designs <pixverse.official00@gmail.com>
2026-06-20 18:12:52 +0530

    Update surah.js

--- js/surah.js
+++ js/surah.js
@@ -9,3 +9,9 @@ document.getElementById("surahContent").innerHTML = `
 Surah content will be added here later.
 </p>
 `;
+
+fetch(`https://api.alquran.cloud/v1/surah/${surahId}`)
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+});
