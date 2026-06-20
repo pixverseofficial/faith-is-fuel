072256602f1869fab52361db8fd289c4184b85d3
vex.designs <pixverse.official00@gmail.com>
2026-06-20 13:59:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -240,3 +240,39 @@ loadStats();
 loadArticles();
 loadReels();
 loadStats();
+const loader = document.querySelector(".page-loader");
+
+window.addEventListener("pageshow", () => {
+    if(loader){
+        loader.classList.remove("show");
+    }
+});
+
+document.querySelectorAll("a").forEach(link => {
+
+    link.addEventListener("click", function(e){
+
+        const href = this.getAttribute("href");
+
+        if(
+            !href ||
+            href.startsWith("#") ||
+            href.startsWith("mailto:") ||
+            href.startsWith("tel:")
+        ){
+            return;
+        }
+
+        e.preventDefault();
+
+        if(loader){
+            loader.classList.add("show");
+        }
+
+        setTimeout(() => {
+            window.location.href = href;
+        }, 1000);
+
+    });
+
+});
