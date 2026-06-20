3a4b45c0a33e79797e292819ab700c81ce3ef477
vex.designs <pixverse.official00@gmail.com>
2026-06-20 17:20:38 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,3 +34,25 @@ document.querySelectorAll("a").forEach(link => {
     });
 
 });
+// page loader code
+...
+
+// navbar scroll effect
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        navbar.style.background = "rgba(0,0,0,.75)";
+        navbar.style.backdropFilter = "blur(30px)";
+        navbar.style.boxShadow = "0 10px 30px rgba(0,0,0,.35)";
+
+    }else{
+
+        navbar.style.background = "rgba(0,0,0,.25)";
+        navbar.style.boxShadow = "none";
+
+    }
+
+});
