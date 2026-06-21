75b9edbe0457320d05e7338f55dc3d55061a02ff
vex.designs <pixverse.official00@gmail.com>
2026-06-21 10:23:29 +0530

    Update surah.js

--- js/surah.js
+++ js/surah.js
@@ -1,28 +1,88 @@
 const params = new URLSearchParams(window.location.search);
 const surahId = params.get("id");
 
-fetch(`https://api.alquran.cloud/v1/surah/${surahId}/quran-uthmani`)
+fetch(`https://api.quran.com/api/v4/quran/verses/uthmani?chapter_number=${surahId}`)
 .then(res => res.json())
 .then(result => {
 
-    const surah = result.data;
+    console.log(result);
+
+    const ayahs = result.verses;
 
     document.getElementById("surahName").innerText =
-    surah.englishName;
+    "Surah " + surahId;
 
-    let content = "";
+    const muqattaat = [
+        "الم","الر","المر","المص",
+        "كهيعص","طه","طس","طسم",
+        "يس","ص","حم","عسق",
+        "ق","ن"
+    ];
 
-    surah.ayahs.forEach(ayah => {
+    let verses = "";
+    verses += `<div class="quran-page">`;
 
-        content += `
+    let ayahsList = [...ayahs];
+
+    if(surahId !== "1" && surahId !== "9"){
+        verses += `
+        <div class="bismillah">
+            بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ
+        </div>
+        `;
+    }
+
+    if(surahId !== "1" && surahId !== "9"){
+        ayahsList[0].text =
+        ayahsList[0].text.replace(
+        "بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ",
+        ""
+        ).trim();
+    }
+
+    let firstAyah = ayahsList[0].text;
+    let muqattaahText = "";
+
+    for(const item of muqattaat){
+        if(firstAyah.startsWith(item)){
+            muqattaahText = item;
+            ayahsList[0].text =
+            firstAyah.replace(item,"").trim();
+            break;
+        }
+    }
+
+    if(muqattaahText){
+        verses += `
+        <div class="muqattaat">
+            ${muqattaahText}
+        </div>
+        `;
+    }
+
+    ayahsList.forEach(ayah => {
+
+        verses += `
         ${ayah.text}
+
         <span class="ayah-number">
-            ${ayah.numberInSurah}
+            ${ayah.verse_number}
         </span>
         `;
+
     });
 
+    verses += `</div>`;
+
     document.getElementById("surahContent").innerHTML =
-    `<div class="quran-page">${content}</div>`;
+    verses;
+
+})
+.catch(error => {
+
+    console.log(error);
+
+    document.getElementById("surahContent").innerHTML =
+    "Failed to load Surah.";
 
 });
