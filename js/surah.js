a9b83ad0ffa3740aaf8c7fb9c4d4bfa71b1ab2f6
vex.designs <pixverse.official00@gmail.com>
2026-06-21 10:17:22 +0530

    Update surah.js

--- js/surah.js
+++ js/surah.js
@@ -1,1 +1,28 @@
+const params = new URLSearchParams(window.location.search);
+const surahId = params.get("id");
 
+fetch(`https://api.alquran.cloud/v1/surah/${surahId}/quran-uthmani`)
+.then(res => res.json())
+.then(result => {
+
+    const surah = result.data;
+
+    document.getElementById("surahName").innerText =
+    surah.englishName;
+
+    let content = "";
+
+    surah.ayahs.forEach(ayah => {
+
+        content += `
+        ${ayah.text}
+        <span class="ayah-number">
+            ${ayah.numberInSurah}
+        </span>
+        `;
+    });
+
+    document.getElementById("surahContent").innerHTML =
+    `<div class="quran-page">${content}</div>`;
+
+});
