31514c9761f7b3bbe50de612d891d12243511716
vex.designs <pixverse.official00@gmail.com>
2026-06-22 17:08:46 +0530

    Update adhkar.js

--- js/adhkar.js
+++ js/adhkar.js
@@ -1,19 +1,33 @@
 const container = document.getElementById("adhkarContainer");
 
+fetch("data/adhkar.json")
+.then(res => res.json())
+.then(data => {
+
+```
+renderAdhkar(data);
+```
+
+});
+
 function renderAdhkar(list){
 
-    container.innerHTML = "";
+```
+container.innerHTML = "";
 
-    list.forEach(item => {
+list.forEach(item => {
 
-        container.innerHTML += `
-            <a href="adhkar-view.html?cat=${item.category.toLowerCase()}" class="card" style="text-decoration:none;color:white;">
+    container.innerHTML += `
+        <a href="adhkar-view.html?cat=${item.category.toLowerCase()}" class="card" style="text-decoration:none;color:white;">
 
-                <h3>${item.title}</h3>
+            <h3>${item.title}</h3>
 
-                <p>${item.translation}</p>
+            <p>${item.translation}</p>
 
-            </a>
-        `;
-    });
+        </a>
+    `;
+
+});
+```
+
 }
