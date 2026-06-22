d115473c6788c95683b7db927971874aff2d90fc
vex.designs <pixverse.official00@gmail.com>
2026-06-22 17:22:53 +0530

    Create adhkar-view.js

---
+++ js/adhkar-view.js
@@ -0,0 +1,24 @@
+const params = new URLSearchParams(window.location.search);
+const category = params.get("cat");
+
+document.getElementById("categoryTitle").innerText = category.toUpperCase() + " ADHKAR";
+
+const adhkarList = document.getElementById("adhkarList");
+
+fetch("data/full-adhkar.json")
+.then(res => res.json())
+.then(data => {
+    const list = data[category] || [];
+    list.forEach(item => {
+        adhkarList.innerHTML += `
+            <div class="adhkar-card">
+                <div class="arabic">
+                    ${item.arabic}
+                </div>
+                <div class="english">
+                    ${item.english}
+                </div>
+            </div>
+        `;
+    });
+});
